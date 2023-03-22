b37879ef56b9c83fd13c0d3cff7ea76e1d0c94e0
Åmund Henriksen <aamundhe@getacademy.no>
2023-03-22 10:31:58 +0100

    refactored

--- Pensumsjekk Uke 2/Class1.cs
+++ Pensumsjekk Uke 2/Class1.cs
@@ -12,34 +12,14 @@ namespace Pensumsjekk_Uke_2
 
         public Class1()
         {
-            students = new List<Student>()
-            {
-                new Student("Nora", 43, "Excel"),
-                new Student("Emma", 22, "Brannsikkerhet"),
-                new Student("Emil", 63, "Maskinfører"),
-                new Student("William", 29, "Fallsikring"),
-                new Student("Filip", 54, "Excel"),
-                new Student("Lucas", 31, "Førstehjelp"),
-                new Student("Ella", 48, "Førstehjelp"),
-                new Student("Jonas", 25, "Stillasbygger"),
-                new Student("Sophie", 23, "Brannsikkerhet")
-            };
-
+            InitializeStudents();
             StartMenu();
         }
 
         public void StartMenu()
         {
             Console.Clear();
-            Console.WriteLine("--- Kurs ---\n");
-
-            Console.WriteLine("Excel");
-            Console.WriteLine("Brannsikkerhet");
-            Console.WriteLine("Maskinfører");
-            Console.WriteLine("Fallsikring");
-            Console.WriteLine("Førstehjelp");
-            Console.WriteLine("Stillasbygger\n");
-
+            AllAvailableCourses();
             Console.WriteLine("Skriv inn et kurs for å se hvem som deltar.");
             var requestedCourse = Console.ReadLine();
             ChosenCourse(requestedCourse);
@@ -79,14 +59,50 @@ namespace Pensumsjekk_Uke_2
             }
         }
 
-        public void ShowCourse(string requestedCourse)
+        public void ShowCourse(string course)
         {
             Console.Clear();
-
-            foreach (var student in students.Where(x => x.Course == requestedCourse))
+            Console.WriteLine($"--- Kurs: {course} ---\n");
+            foreach (var student in students.Where(x => x.Course == course))
             {
                 Console.WriteLine(student.Name);
             }
+            Console.WriteLine("\nTrykk ESC for å gå tilbake til start-menyen...");
+            var input = Console.ReadKey(true);
+
+            while (input.Key != ConsoleKey.Escape)
+            {
+                input = Console.ReadKey(true);
+            }
+            StartMenu();
+        }
+
+        public void InitializeStudents()
+        {
+            students = new List<Student>()
+            {
+                new Student("Nora", 43, "Excel"),
+                new Student("Emma", 22, "Brannsikkerhet"),
+                new Student("Emil", 63, "Maskinfører"),
+                new Student("William", 29, "Fallsikring"),
+                new Student("Filip", 54, "Excel"),
+                new Student("Lucas", 31, "Førstehjelp"),
+                new Student("Ella", 48, "Førstehjelp"),
+                new Student("Jonas", 25, "Stillasbygger"),
+                new Student("Sophie", 23, "Brannsikkerhet")
+            };
+        }
+
+        public void AllAvailableCourses()
+        {
+            Console.WriteLine("--- Kurs ---\n");
+
+            Console.WriteLine("Excel");
+            Console.WriteLine("Brannsikkerhet");
+            Console.WriteLine("Maskinfører");
+            Console.WriteLine("Fallsikring");
+            Console.WriteLine("Førstehjelp");
+            Console.WriteLine("Stillasbygger\n");
         }
     }
 }
